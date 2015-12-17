6f9799d161101d9c414e3c22efafa0f59c95c58c
Joao FSE <jofisaes@hotmail.com>
2015-12-17 13:52:44 +0100

    SZ7 Usage of lambda java 8 expressions

--- vma-mvc/src/main/java/com/steelzack/vma/controller/mapper/OriginalInfoMapper.java
+++ vma-mvc/src/main/java/com/steelzack/vma/controller/mapper/OriginalInfoMapper.java
@@ -1,5 +1,6 @@
 package com.steelzack.vma.controller.mapper;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 
 import com.steelzack.vma.controller.pojo.OriginalInfo;
@@ -7,4 +8,7 @@ import com.steelzack.vma.controller.pojo.OriginalInfo;
 public interface OriginalInfoMapper {
 	@Insert("INSERT into OriginalInfo(id,title) VALUES(#{id}, #{title})")
 	void insertOriginalInfo(OriginalInfo originalInfo);
+
+	@Delete("delete from OriginalInfo")
+	void deleteAllOriginalInfo();
 }
--- vma-mvc/src/test/java/com/steelzack/vma/controller/mapper/OriginalInfoMapperTest.java
+++ vma-mvc/src/test/java/com/steelzack/vma/controller/mapper/OriginalInfoMapperTest.java
@@ -1,5 +1,11 @@
 package com.steelzack.vma.controller.mapper;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 import org.junit.Test;
@@ -27,6 +33,48 @@ public class OriginalInfoMapperTest implements OriginalInfoMapper {
 			originalInfo.setTitle("This is a test title");
 			mapper.insertOriginalInfo(originalInfo);
 			log.info("---Original title saved---");
+			mapper.deleteAllOriginalInfo();
 		}
+	}
+
+	@Test
+	public void testMapping_List() throws Exception {
+		try (final AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext()) {
+			ctx.register(AppConfig.class);
+			ctx.refresh();
+			final OriginalInfoMapper mapper = ctx.getBean(OriginalInfoMapper.class);
+			mapper.deleteAllOriginalInfo();
+
+			final int nElements = 3;
+			Double avg = 0d;
+			List<OriginalInfo> originalInfos = new ArrayList<OriginalInfo>();
+			for (int currentElement = 0; currentElement < nElements; currentElement++) {
+				OriginalInfo originalInfo = new OriginalInfo();
+				originalInfo.setId(currentElement + 1);
+				originalInfo.setTitle("This is a test title -" + UUID.randomUUID().toString());
+				originalInfo.setDuration(new Random().nextDouble());
+				mapper.insertOriginalInfo(originalInfo);
+				log.info("---Original title saved---");
+
+				originalInfos.add(originalInfo);
+				avg += originalInfo.getDuration();
+				log.info(String.format("Average for element %d was %f", currentElement, originalInfo.getDuration()));
+			}
+			avg = avg / nElements;
+
+			Double resultAvg = originalInfos.stream(). //
+					mapToDouble(o -> o.getDuration()). //
+					average(). //
+					getAsDouble();
+
+			assertEquals(avg, resultAvg);
+			log.info("Avergage found was:" + resultAvg);
+			mapper.deleteAllOriginalInfo();
+		}
+	}
+
+	@Override
+	public void deleteAllOriginalInfo() {
+		log.info(String.format("All information as been deleted!"));
 	}
 }
