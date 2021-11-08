94161271f89692b275fee402dc3ff3ab6c9c5b17
Joao Esperancinha <jofisaes@gmail.com>
2021-11-08 19:20:54 +0100

    🌿 1 WRITE - 2 READS

--- docker-images/psql/entrypoint.sh
+++ docker-images/psql/entrypoint.sh
@@ -5,6 +5,7 @@ cd /var/lib/postgresql || exit
 sed -i 's/${VMA_ETCD_IP}/'"$VMA_ETCD_IP"'/g' /var/lib/postgresql/patroni.yml
 sed -i 's/${POSTGRES_MAIN_IP}/'"$POSTGRES_MAIN_IP"'/g' /var/lib//postgresql/patroni.yml
 sed -i 's/${POSTGRES_SECOND_IP}/'"$POSTGRES_SECOND_IP"'/g' /var/lib/postgresql/patroni.yml
+sed -i 's/${POSTGRES_THIRD_IP}/'"$POSTGRES_THIRD_IP"'/g' /var/lib/postgresql/patroni.yml
 sed -i 's/${POSTGRES_NUMBER}/'"$POSTGRES_NUMBER"'/g' /var/lib/postgresql/patroni.yml
 
 su - postgres -c "/var/lib/postgresql/command.sh"
