e381043f6ffbc5878fb7812f09d166588f73cc13
Joao Esperancinha <jofisaes@gmail.com>
2022-02-27 10:00:03 +0100

    👾 Clean Up

--- vma-service-backend/src/test/kotlin/org/jesperancinha/vma/VmaServiceApplicationTests.kt
+++ vma-service-backend/src/test/kotlin/org/jesperancinha/vma/VmaServiceApplicationTests.kt
@@ -1,10 +1,8 @@
 package org.jesperancinha.vma
 
-import com.ninjasquad.springmockk.MockkBean
 import org.jesperancinha.vma.utils.AbstractVmaTest
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.web.reactive.error.ErrorAttributes
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
