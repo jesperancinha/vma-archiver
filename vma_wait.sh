3f9b32937c3f1ed1e3a510b96c034977a23aee45
Joao Esperancinha <jofisaes@gmail.com>
2022-07-05 09:36:45 +0200

    Fixes VMA context path

--- vma_wait.sh
+++ vma_wait.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
-docker logs staco_app_service_localstack &> "logs"
+docker logs jofisaes_vma_backend_img_1 &> "logs"
 string=$(cat logs)
 counter=0
-#while [[ "$string" != *"Bucket(Name=stacos"* ]]
-#do
-#  printf "."
-#  docker logs staco_app_service_localstack &> "logs"
-#  string=$(cat logs)
-#  sleep 1
-#  counter=$((counter+1))
-#  if [ $counter -eq 200 ]; then
-#      echo "Failed after $counter tries! All cypress tests should fail!"
-#      exit
-#  fi
-#done
-echo "Succeeded after $counter tries!"
+while [[ "$string" != *"Started VmaServiceApplicationKt"* ]]
+do
+  printf "."
+  docker logs jofisaes_vma_backend_img_1 &> "logs"
+  string=$(cat logs)
+  sleep 1
+  counter=$((counter+1))
+  if [ $counter -eq 200 ]; then
+      echo "Failed after $counter tries! All cypress tests should fail!"
+      exit
+  fi
+done
+counter=$((counter+1))
+echo "Succeeded VMA BE 1 Service after $counter tries!"
+
+docker logs jofisaes_vma_backend_img_2 &> "logs"
+string=$(cat logs)
+counter=0
+while [[ "$string" != *"Started VmaServiceApplicationKt"* ]]
+do
+  printf "."
+  docker logs jofisaes_vma_backend_img_2 &> "logs"
+  string=$(cat logs)
+  sleep 1
+  counter=$((counter+1))
+  if [ $counter -eq 200 ]; then
+      echo "Failed after $counter tries! All cypress tests should fail!"
+      exit
+  fi
+done
+counter=$((counter+1))
+echo "Succeeded VMA BE 2 Service after $counter tries!"
