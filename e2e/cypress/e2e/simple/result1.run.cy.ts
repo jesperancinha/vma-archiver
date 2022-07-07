0245c397c06176711fe2a560cd9ebfccbf2401c7
Joao Esperancinha <jofisaes@gmail.com>
2022-07-07 23:47:59 +0200

    More Cypress tests

--- e2e/cypress/e2e/simple/result1.run.cy.ts
+++ e2e/cypress/e2e/simple/result1.run.cy.ts
@@ -2,7 +2,7 @@ chai.use(require('chai-uuid'));
 
 describe('Results #1', () => {
     it('passes', () => {
-        cy.visit('http://localhost:8080/result')
+        cy.visit('http://localhost:8080/result');
         cy.get('mat-card-title').contains('VMA - Voting results!')
             .should((matCardTitle) => {
                 const text = matCardTitle.text();
