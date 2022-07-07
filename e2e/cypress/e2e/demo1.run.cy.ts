044c00ad3ca661c78a88b2964423740ccd478d85
Joao Esperancinha <jofisaes@gmail.com>
2022-07-07 23:13:00 +0200

    Tests first selection on radio buttons

--- e2e/cypress/e2e/demo1.run.cy.ts
+++ e2e/cypress/e2e/demo1.run.cy.ts
@@ -2,18 +2,25 @@ chai.use(require('chai-uuid'));
 
 describe('Vote Casting #1', () => {
     it('passes', () => {
-        cy.visit('http://localhost:8080')
+        cy.visit('http://localhost:8080');
+        cy.wait(1000);
         cy.get('mat-card-title').contains('VMA - Cast your vote page!')
             .should((matCardTitle) => {
-                const text = matCardTitle.text()
-                expect(text).to.eq('VMA - Cast your vote page!')
+                const text = matCardTitle.text();
+                expect(text).to.eq('VMA - Cast your vote page!');
             });
         cy.get('mat-card-title').contains('VMA - Cast your vote page!')
             .next().children().first().should((matCardTitle) => {
-            const text = matCardTitle.text()
-            expect(text).to.be.a.uuid()
+            const text = matCardTitle.text();
+            expect(text).to.be.a.uuid();
         });
         cy.get('mat-card-title').contains('VMA - Cast your vote page!')
-            .next().children().first().next().children().should('have.length.gte', 4)
+            .next().children().first().next().children().should('have.length.gte', 4);
+
+        cy.titlesCheck('VMA - Cast your vote page!');
+        cy.wait(1000);
+        cy.selectFirst('VMA - Cast your vote page!');
+        cy.get('mat-card-title').next().get('button').click();
+        cy.titlesCheck('VMA - Cast your vote page!');
     })
 })
--- e2e/cypress/e2e/result1.run.cy.ts
+++ e2e/cypress/e2e/result1.run.cy.ts
@@ -5,15 +5,18 @@ describe('Results #1', () => {
         cy.visit('http://localhost:8080/result')
         cy.get('mat-card-title').contains('VMA - Voting results!')
             .should((matCardTitle) => {
-                const text = matCardTitle.text()
-                expect(text).to.eq('VMA - Voting results!')
+                const text = matCardTitle.text();
+                expect(text).to.eq('VMA - Voting results!');
             });
         cy.get('mat-card-title').contains('VMA - Voting results!')
             .next().children().first().should((matCardTitle) => {
-            const text = matCardTitle.text()
-            expect(text).to.be.a.uuid()
+            const text = matCardTitle.text();
+            expect(text).to.be.a.uuid();
         });
-        cy.get('mat-card-title').contains('VMA - Voting results!')
-            .next().children().first().next().children().should('have.length.gte', 4)
+        let allVotingResults = cy.get('mat-card-title').contains('VMA - Voting results!')
+            .next().children().first().next().children();
+        allVotingResults.should('have.length.gte', 4);
+
+        cy.titlesCheck('VMA - Voting results!');
     })
 })
