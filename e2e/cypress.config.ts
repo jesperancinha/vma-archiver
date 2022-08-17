cfa562aa30328a58dc403ca5febbb98eb754718c
Joao Esperancinha <jofisaes@gmail.com>
2022-08-17 17:53:10 +0200

    Serial E2E tests

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "cypress";
+import {defineConfig} from "cypress";
 
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+module.exports = defineConfig({
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        env: {
+            TIMEOUT_CONFIG: {
+                timeout: 10000
+            }
+        }
     },
-  },
 });
--- e2e/cypress/e2e/simple/demo1.run.cy.ts
+++ e2e/cypress/e2e/simple/demo1.run.cy.ts
@@ -1,28 +1,30 @@
 chai.use(require('chai-uuid'));
 
+const TIMEOUT_CONFIG = Cypress.env('TIMEOUT_CONFIG');
 describe('Vote Casting #1', () => {
     it('passes', () => {
-        let host = Cypress.env('host') ?  Cypress.env('host') : 'localhost';
+        cy.log(TIMEOUT_CONFIG);
+        let host = Cypress.env('host') ? Cypress.env('host') : 'localhost';
         cy.visit(`http://${host}:8080`);
         cy.wait(1000);
-        cy.get('mat-card-title').contains('VMA - Cast your vote page!')
+        cy.get("mat-card-title", TIMEOUT_CONFIG).contains('VMA - Cast your vote page!', TIMEOUT_CONFIG)
             .should((matCardTitle) => {
                 const text = matCardTitle.text();
                 expect(text).to.eq('VMA - Cast your vote page!');
             });
-        cy.get('mat-card-title').contains('VMA - Cast your vote page!')
+        cy.get("mat-card-title", TIMEOUT_CONFIG).contains('VMA - Cast your vote page!', TIMEOUT_CONFIG)
             .next().children().first().should((matCardTitle) => {
             const text = matCardTitle.text();
             expect(text).to.be.a.uuid();
         });
-        cy.get('mat-card-title').contains('VMA - Cast your vote page!')
+        cy.get("mat-card-title", TIMEOUT_CONFIG).contains("VMA - Cast your vote page!", TIMEOUT_CONFIG)
             .next().children().first().next().children().should('have.length.gte', 4);
 
         cy.titlesCheck('VMA - Cast your vote page!');
         cy.wait(1000);
         cy.selectFirst('VMA - Cast your vote page!');
         const click = $el => $el.click()
-        cy.get('mat-card-title').next().get('button').pipe(click);
+        cy.get("mat-card-title").next().get("button", TIMEOUT_CONFIG).pipe(click);
         cy.titlesCheck('VMA - Cast your vote page!');
     })
 })
