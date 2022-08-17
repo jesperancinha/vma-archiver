e5dfadf657fa5791fcb0e3f7489f95d1675c1919
Joao Esperancinha <jofisaes@gmail.com>
2022-08-17 18:49:44 +0200

    const TIMEOUT_CONFIG

--- e2e/cypress/e2e/full/full1.run.cy.ts
+++ e2e/cypress/e2e/full/full1.run.cy.ts
@@ -1,28 +1,29 @@
 chai.use(require('chai-uuid'));
 
 describe('Vote Casting #1', () => {
+    const TIMEOUT_CONFIG = Cypress.env('TIMEOUT_CONFIG');
     it('passes', () => {
         let host = Cypress.env('host') ?  Cypress.env('host') : 'localhost';
         cy.visit(`http://${host}:8080`);
         cy.wait(1000);
-        cy.get('mat-card-title').contains('VMA - Cast your vote page!')
+        cy.get('mat-card-title', TIMEOUT_CONFIG).contains('VMA - Cast your vote page!')
             .should((matCardTitle) => {
                 const text = matCardTitle.text();
                 expect(text).to.eq('VMA - Cast your vote page!');
             });
-        cy.get('mat-card-title').contains('VMA - Cast your vote page!')
+        cy.get('mat-card-title', TIMEOUT_CONFIG).contains('VMA - Cast your vote page!')
             .next().children().first().should((matCardTitle) => {
             const text = matCardTitle.text();
             expect(text).to.be.a.uuid();
         });
-        cy.get('mat-card-title').contains('VMA - Cast your vote page!')
+        cy.get('mat-card-title', TIMEOUT_CONFIG).contains('VMA - Cast your vote page!')
             .next().children().first().next().children().should('have.length.gte', 4);
 
         cy.titlesCheck('VMA - Cast your vote page!');
         cy.wait(1000);
         cy.selectFirst('VMA - Cast your vote page!');
         const click = $el => $el.click()
-        cy.get('mat-card-title').next().get('button')
+        cy.get('mat-card-title', TIMEOUT_CONFIG).next().get('button', TIMEOUT_CONFIG)
             .contains("Cast Votes!").pipe(click);
         cy.wait(1000)
         cy.titlesCheck('VMA - Cast your vote page!');
