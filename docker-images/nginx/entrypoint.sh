c879b3e74d901da6b7118a9a7066ccd407d035e3
Joao Esperancinha <jofisaes@gmail.com>
2023-01-13 12:06:08 +0100

    Updates images to not use fixed IP address

--- docker-images/nginx/entrypoint.sh
+++ docker-images/nginx/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sed -i 's/${VMA_BACKEND_1}/'"$(getent hosts jofisaes-vma-backend-img-1 | cut -d' ' -f1)"'/g' /etc/nginx/nginx.conf
+sed -i 's/${VMA_BACKEND_2}/'"$(getent hosts jofisaes-vma-backend-img-2 | cut -d' ' -f1)"'/g' /etc/nginx/nginx.conf
+sed -i 's/${VMA_SOCKETS_1}/'"$(getent hosts jofisaes-vma-websockets-1 | cut -d' ' -f1)"'/g' /etc/nginx/nginx.conf
+sed -i 's/${VMA_SOCKETS_2}/'"$(getent hosts jofisaes-vma-websockets-2 | cut -d' ' -f1)"'/g' /etc/nginx/nginx.conf
+
 nginx -t
 
 nginx
