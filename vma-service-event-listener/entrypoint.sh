87cde1942b2f2a8e3d56fbdf321d249c43321ae8
jesperancinha <jofisaes@gmail.com>
2025-01-23 11:33:58 +0100

    bash to sh

--- vma-service-event-listener/entrypoint.sh
+++ vma-service-event-listener/entrypoint.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 java -jar -Dapplication.schema.host=${VMA_SCHEMA_REGISTRY} -Dapplication.postgres.host=jofisaes-vma-haproxy-lb -Dspring.profiles.active=docker -Dserver.port="${VMA_PORT}" vma-service-event-listener.jar
--- vma-service-websockets/entrypoint.sh
+++ vma-service-websockets/entrypoint.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 java -jar -Dapplication.schema.host=${VMA_SCHEMA_REGISTRY} -Dapplication.postgres.host=jofisaes-vma-haproxy-lb -Dspring.profiles.active=${VMA_BACKEND_PROFILE} -Dserver.port="${VMA_PORT}" vma-service-websockets.jar
